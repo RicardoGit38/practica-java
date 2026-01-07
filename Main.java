9f991499cf057b28c785205f33690e692ff28243
RicardoGit38 <ricardo.champi@outlook.com>
2026-01-07 15:35:51 -0500

    nuevo mensaje

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
        System.out.println("Hello, World! edited from my PC");
 
-       
+       System.out.println("Hola, Mundo! editado desde mi PC gamer");
     }
 }
